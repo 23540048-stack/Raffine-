75c8cb61fbbfdb4508c4e383658ee0720c14fd99
Vy <23540048@gm.uit.edu.vn>
2025-11-12 17:49:42 +0700

    Update script path

--- js/women-page.js
+++ js/women-page.js
@@ -154,3 +154,19 @@ document.addEventListener("DOMContentLoaded", () => {
     allProducts.forEach((p) => (p.style.display = "block"));
   }
 });
+
+// Click vào sản phẩm sẽ chuyển sang trang chi tiết sản phẩm
+
+document.addEventListener("DOMContentLoaded", function () {
+  const productCards = document.querySelectorAll(".product-card");
+
+  productCards.forEach((card) => {
+    card.addEventListener("click", () => {
+      const id = card.getAttribute("data-id");
+      if (id) {
+        // Chuyển sang trang chi tiết sản phẩm
+        window.location.href = `product-detail-page.html?id=${id}`;
+      }
+    });
+  });
+});
